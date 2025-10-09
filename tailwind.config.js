62a5b0c6adf43f938e9c7b4b27f57da9d372b909
sangeetha-283 <sagie.poorni@gmail.com>
2025-10-09 10:59:47 +0530

    Updated greenden project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content:[
+        './**/*.html',
+        './**/*.js',
+        //Add any other file types or patterns that contain your tailwind CSS classes
+    ],
+    theme:{
+        extend:{},
+    },
+        plugins:[],
+ }
+
+
+ 
